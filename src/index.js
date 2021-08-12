e1c12e40e0f96531cb85e00a859aed3643bae149
cwilly89 <88181324+cwilly89@users.noreply.github.com>
2021-08-12 00:22:07 -0700

    integrated api to show current temp

--- src/index.js
+++ src/index.js
@@ -1,36 +1,17 @@
-function formatDate(timestamp) {
-  let date = new Date(timestamp);
-  let hours = date.getHours();
-  let minutes = date.getMinutes();
-  if (minutes)
-  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursaday", "Friday", "Saturday"]
-  let day = days[date.getDay()];
-  return `${day} ${hours}:${minutes}`;
-  
-
-}
-
-
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
-  let dateElement = document.querySelector("#date");
-  let iconElement = document.querySelector("#icon");
-
-  temperatureElement.innerHTMl = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
   descriptionElement.innerHTML = response.data.weather[0].description;
-  humidityElement.innerHTML = response.main.humidity;
+  humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
-  dateElement.innerHTML = formatDate(response.data.dt * 1000);
-  iconElement.setAttribute("src",`http://openweathermap.org/img/wn/10d@2x.png`);
 }
 
 let apiKey = "a4748acc18b1b91de37c3e8310fc0ce3";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=imperial`;
-let city = "sydney"
 
 axios.get(apiUrl).then(displayTemperature);
